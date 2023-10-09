8ffad618c315b9f2f3f829ea8f30b3f2b277db07
safwan-beriwala <147388417+safwan-beriwala@users.noreply.github.com>
2023-10-09 11:27:55 +0000

    VARIABLES

---
+++ 01_basic/01_variable.js
@@ -0,0 +1,12 @@
+const accoundId=144553
+let accountEmail="safwan@gmail.com"
+var accountPassword="12345"
+accountCity="jaipur"
+
+accountEmail="zaid@gmail.com"
+accountPassword="12121"
+accountCity="AHMEDABAD"
+
+console.table([accountEmail,accountPassword,accountCity])
+//NEVER USED VAR IN JAVASCRIPT
+//ONLY USE CONST AND LET IN TODAYS JAVASCRIPT
