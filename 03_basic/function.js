b51f2838fbea60d71b3873fabe4ecdb54ce63139
safwan-beriwala <147388417+safwan-beriwala@users.noreply.github.com>
2023-10-19 10:17:57 +0000

    function

--- 03_basic/function.js
+++ 03_basic/function.js
@@ -1,6 +1,29 @@
-function addTwoNumbers(n1,n2){
-let result=n1+n2
-return result
+// function addTwoNumbers(n1,n2){
+// let result=n1+n2
+// return result
+// }
+// const result=addTwoNumbers(10,20)
+// console.log("RESULT IS:",result)
+
+//PARAMETER KAISE LIYE JATY HAI
+function userLoginIn(username){
+    return `${username} just logged in`
 }
-const result=addTwoNumbers(10,20)
-console.log("RESULT IS:",result)
+console.log(userLoginIn("SAFWAN"))
+
+const user={
+    name:"sawan  beriwala",
+    password:123123
+}
+function check (passobj){
+    return `User name is ${passobj.name} and password is ${passobj.password}`
+}
+
+console.log(check(user))
+
+const array=[100,200,300,400,500]
+
+function arr(getarr){
+    return getarr[2]
+}
+console.log(arr(array))
