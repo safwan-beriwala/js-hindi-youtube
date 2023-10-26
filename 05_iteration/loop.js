8d6b208392a447564485e8e0df38d00985d62c24
safwan-beriwala <147388417+safwan-beriwala@users.noreply.github.com>
2023-10-26 12:38:48 +0000

    for,while,do-while lops

---
+++ 05_iteration/loop.js
@@ -0,0 +1,59 @@
+// for(let i=0;i<=10;i++){
+//     if(i==5){
+//         console.log("5 is best number")
+//     }
+//     console.log(i)
+// }
+
+// for(let i=1;i<=10;i++){
+//     console.log(`outer loop value ${i}`)
+
+//     for(let j=1;j<=10;j++){
+//         // console.log(`inner loop value ${j} and outer loop is ${i}`)
+//         console.log(i + '*' +j+'='+i*j)
+//     }
+
+//     }
+
+// BREAK
+
+// for (let index = 1; index <=20; index++) {
+//    if(index==5){
+//     console.log("5 is bes number detected");
+//     break
+//    }
+   
+//     console.log(index)
+// }
+
+//CONTINUE
+// for (let index = 1; index<=20; index++){
+
+//     if(index==5){
+//         console.log("5 number detedted")
+//         continue
+//     }
+//    console.log(index)
+// }
+
+                                //    WHILE LOOP
+// let i=0
+// while (i<=10) {
+//     console.log(`value of i is ${i}`)
+//     i=i+1
+// }
+
+// let myarray=["flash","superman","batman","spiderman","ironman","thor"]
+// let arr=0
+// while (arr<myarray.length) {
+//     console.log(myarray[arr])
+//    arr=arr+1
+// }
+
+                                    //DO-WHILE-LOOP
+let score=11
+
+ do {
+    console.log(`value of score is ${score}`)
+    score=score+1
+} while (score<=10);
