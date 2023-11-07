37f7e0c909b6d181114996aa749b7bc4b4330e6b
safwan-beriwala <147388417+safwan-beriwala@users.noreply.github.com>
2023-11-07 10:12:54 +0000

    MAP

---
+++ 06_filter_map_reduce/map.js
@@ -0,0 +1,12 @@
+                                    //MAps
+
+
+ const mynumbers=[1,2,3,4,5,6,7,8,9,10]
+// const mynums=mynumbers.map((num)=>num +10)
+// console.log(mynums)
+
+//CHAINNIG IN JS USING MAP
+const newnums=mynumbers.map((num)=> num*10)
+                       .map((num)=> num+1)
+                       .filter((num)=> num >=40)
+                       console.log(newnums)
